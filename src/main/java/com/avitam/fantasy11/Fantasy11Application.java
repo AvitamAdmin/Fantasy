67558d8ef914f85660c9f952be011a415c1816c7
Harshu-B <107806758+Harshu-B@users.noreply.github.com>
2024-04-18 14:18:45 +0530

    initial

---
+++ src/main/java/com/avitam/fantasy11/Fantasy11Application.java
@@ -0,0 +1,13 @@
+package com.avitam.fantasy11;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Fantasy11Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Fantasy11Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/avitam/fantasy11/Fantasy11ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.avitam.fantasy11;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Fantasy11ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
