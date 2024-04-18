c3abacb3d076cc850c977e659d7f0c3fa75fb3b1
bhuvanesh <bhuvanesh@avitam.in>
2024-04-18 18:30:59 +0530

    BaseEntity added

--- src/main/java/com/avitam/fantasy11/model/UserRepository.java
+++ src/main/java/com/avitam/fantasy11/model/UserRepository.java
@@ -2,5 +2,5 @@ package com.avitam.fantasy11.model;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
 
-public interface UserRepository extends MongoRepository<User, Long> {
+public interface UserRepository extends MongoRepository<User, Integer> {
 }
---
+++ src/main/java/com/avitam/fantasy11/model/VerificationToken.java
@@ -0,0 +1,20 @@
+package com.avitam.fantasy11.model;
+
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Document("VerificationToken")
+public class VerificationToken {
+
+    private int id;
+    private int userid;
+    private int token;
+    private int status;
+
+}
