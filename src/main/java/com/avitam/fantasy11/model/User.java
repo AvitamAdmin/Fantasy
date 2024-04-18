16c3e36fe786f03e4e5ad96bc3e101dfc3fa0a1c
bhuvanesh <bhuvanesh@avitam.in>
2024-04-18 17:50:06 +0530

    User class, pom.xml

---
+++ src/main/java/com/avitam/fantasy11/model/User.java
@@ -0,0 +1,31 @@
+package com.avitam.fantasy11.model;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.mail.Multipart;
+
+@Document("User")
+@Getter
+@Setter
+@NoArgsConstructor
+public class User {
+
+    private String name;
+    private String emailId;
+    private String password;
+    private String referredBy;
+    private double balance;
+    private double bonus;
+    private String mobileNumber;
+    private String dateOfBirth;
+    private int status;
+    private int role;
+    private MultipartFile profileImage;
+    private String gender;
+    private String language;
+
+}
