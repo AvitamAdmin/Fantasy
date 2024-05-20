7c9126c67cbb2919790e921a356edd0e633c98ed
MuthuRathnam <muthurathinam@avitam.in>
2024-05-20 14:09:53 +0530

    Base Changes done and Model Added

--- src/main/java/com/avitam/fantasy11/model/ContestJoinedRepository.java
+++ src/main/java/com/avitam/fantasy11/model/ContestJoinedRepository.java
@@ -1,9 +1,8 @@
 package com.avitam.fantasy11.model;
 
-import org.bson.types.ObjectId;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ContestJoinedRepository extends MongoRepository<ContestJoined, ObjectId> {
+public interface ContestJoinedRepository extends MongoRepository<ContestJoined, Long> {
 }
