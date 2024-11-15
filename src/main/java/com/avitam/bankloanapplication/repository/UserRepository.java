8377df3ef6b9b4182c5e450b02b8826979b24001
bhuvanesh <bhuvanesh@avitam.in>
2024-11-15 16:04:49 +0530

    commit1

---
+++ src/main/java/com/avitam/bankloanapplication/repository/UserRepository.java
@@ -0,0 +1,23 @@
+package com.avitam.bankloanapplication.repository;
+
+import com.avitam.bankloanapplication.model.entity.User;
+import org.bson.types.ObjectId;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends MongoRepository<User, ObjectId> {
+
+    // JPQL
+    boolean existsByUsername(String username);
+
+    User findByUsername(String username);
+
+    void deleteByUsername(String username);
+
+    User findByEmail(String username);
+
+    User findByMobileNumber(String username);
+
+    User findByResetPasswordToken(String token);
+}
