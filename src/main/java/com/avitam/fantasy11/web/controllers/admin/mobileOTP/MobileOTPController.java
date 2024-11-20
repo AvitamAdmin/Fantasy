81d8a51bae88300871e2f4301eae86d01752f580
Sreesharan-Natarajan <sreesharan@avitam.in>
2024-11-20 13:48:52 +0530

    emailOtp generation

--- src/main/java/com/avitam/fantasy11/web/controllers/admin/mobileOTP/MobileOTPController.java
+++ src/main/java/com/avitam/fantasy11/web/controllers/admin/mobileOTP/MobileOTPController.java
@@ -1,4 +1,29 @@
 package com.avitam.fantasy11.web.controllers.admin.mobileOTP;
 
+import com.avitam.fantasy11.api.dto.UserDto;
+import com.avitam.fantasy11.api.service.MobileOTPService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/admin/mobile")
 public class MobileOTPController {
+
+    @Autowired
+    private MobileOTPService mobileOTPService;
+
+    @PostMapping("/send-otp")
+    public UserDto sendOtp(@RequestBody UserDto userDto) {
+
+        return mobileOTPService.sendOtp(userDto);
+    }
+
+    @PostMapping("/validate-otp")
+    public UserDto validateOtp(@RequestBody UserDto userDto) {
+
+        return mobileOTPService.validateOtp(userDto);
+    }
 }
