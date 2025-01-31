edec67e1bf9915aab110b49634d59fad3e63d682
monisha <muthurathinam@avitam.in>
2025-01-31 16:19:00 +0530

    some controllers are changed

--- src/main/java/com/avitam/fantasy11/repository/NodeRepository.java
+++ src/main/java/com/avitam/fantasy11/repository/NodeRepository.java
@@ -10,7 +10,7 @@ import java.util.Optional;
 @Repository("NodeRepository")
 public interface NodeRepository extends GenericImportRepository<Node> {
 
-    List<Node> findByParentNodeId(String id);
+    List<Node> findByParentNode(String id);
 
     Optional<Node> findById(String id);
 
@@ -23,4 +23,8 @@ public interface NodeRepository extends GenericImportRepository<Node> {
     List<Node> findByStatusOrderByDisplayPriority(boolean b);
 
     List<Node> findByParentNode_RecordId(String recordId);
+
+    Object findByParentNode(Object o);
+
+    List<Node> findByParentNode_ParentNodeId(String parentNodeId);
 }
