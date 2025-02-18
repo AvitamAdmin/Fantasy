bc833928209b34c1ef4734597c202de97fe64e9f
Santhosh-6556 <santhosh.gunaseelan@avitam.in>
2025-02-18 18:46:22 +0530

    contest code changed

---
+++ src/main/java/com/avitam/fantasy11/api/dto/RankPriceDto.java
@@ -0,0 +1,16 @@
+package com.avitam.fantasy11.api.dto;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@ToString
+public class RankPriceDto {
+    private String rank;
+    private Integer winPercent;
+    private Double amount;
+}
